d24af7b6ec021ddd1f7f5b4b3b33bdacd20a1f0f
Konstantinos <kongallis@gmail.com>
2020-10-13 12:12:54 +0300

    Added style to todo-list component

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-todo-list',
+  selector: 'todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.css']
 })
