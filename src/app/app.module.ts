d03eeb72151f69c1d546345a5ec880f5afa254b0
Konstantinos <kongallis@gmail.com>
2020-10-13 12:43:33 +0300

    Created add todo functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { TodoListComponent } from './components/todo-list/todo-list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -7,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TodoListComponent implements OnInit {
   todos: object[];
+  todoTitle: string;
+  idForTodo: number;
 
   constructor() { }
 
   ngOnInit(): void {
+    this.todoTitle = '';
+    this.idForTodo = 4;
     this.todos = [
       {
         'id': 1,
@@ -32,5 +36,22 @@ export class TodoListComponent implements OnInit {
       },
     ];
   }
+
+  // Adds a todo to the list
+  addTodo(): void {
+
+    if (this.todoTitle.trim().length === 0) {
+      return;
+    }
+    this.todos.push({
+      id: 4,
+      title: this.todoTitle,
+      completed: false,
+      editing: false
+    });
+    this.todoTitle = '';
+    this.idForTodo++;
+  }
+
 
 }
