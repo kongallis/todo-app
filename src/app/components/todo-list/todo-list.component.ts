07bd3875812bdabfb5638b579502eecdb07dce37
Konstantinos <kongallis@gmail.com>
2020-10-13 12:27:45 +0300

    Created basic template for listing todo  items

--- src/app/components/todo-list/todo-list.component.ts
+++ src/app/components/todo-list/todo-list.component.ts
@@ -6,10 +6,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./todo-list.component.css']
 })
 export class TodoListComponent implements OnInit {
+  todos: object[];
 
   constructor() { }
 
   ngOnInit(): void {
+    this.todos = [
+      {
+        'id': 1,
+        'title': 'Finish Angular Screencast',
+        'completed': false,
+        'editing': false,
+      },
+      {
+        'id': 2,
+        'title': 'Take over world',
+        'completed': false,
+        'editing': false,
+      },
+      {
+        'id': 3,
+        'title': 'One more thing',
+        'completed': false,
+        'editing': false,
+      },
+    ];
   }
 
 }
